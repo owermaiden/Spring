bc51a32f737fb1463a3da2bc938c8b43d2a3f6de
owermaiden <ower.maiden@cybertekschool.com>
2021-11-01 16:06:35 +0300

    Tighly Coupled

---
+++ Spring-00-TighlyCoupled/src/FullTimeMentor.java
@@ -0,0 +1,6 @@
+public class FullTimeMentor {
+    public void createAccount(){
+        System.out.println("Full Time Mentor Account is created...");
+    }
+}
+
---
+++ Spring-00-TighlyCoupled/src/PartTimeMentor.java
@@ -0,0 +1,6 @@
+public class PartTimeMentor {
+    public void createAccount(){
+        System.out.println("Part Time Mentor Account is created...");
+    }
+}
+
