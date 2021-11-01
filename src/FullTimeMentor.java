95566130b62e26b7d4a7dcc04624db2b44f1b87e
owermaiden <93406612+owermaiden@users.noreply.github.com>
2021-11-01 16:09:00 +0300

    Delete src directory

--- src/FullTimeMentor.java
+++
@@ -1,5 +0,0 @@
-public class FullTimeMentor {
-    public void createAccount(){
-        System.out.println("Full Time Mentor Account is created...");
-    }
-}
--- src/PartTimeMentor.java
+++
@@ -1,5 +0,0 @@
-public class PartTimeMentor {
-    public void createAccount(){
-        System.out.println("Part Time Mentor Account is created...");
-    }
-}
