0a677184aaee7bf963bb671a871a835e010642c0
owermaiden <ower.maiden@cybertekschool.com>
2021-11-01 16:26:36 +0300

    Lousely Coupled

--- Spring-00-LouselyCoupled/src/CybertekApp.java
+++ Spring-00-LouselyCoupled/src/CybertekApp.java
@@ -1,12 +1,14 @@
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
 
         FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
 
-        MentorAccount mentor = new MentorAccount(fullTime);
+        MentorAccount mentor = new MentorAccount(partTimeMentor);
 
         mentor.manageAccount();
     }
