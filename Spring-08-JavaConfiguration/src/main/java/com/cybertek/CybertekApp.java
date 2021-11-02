1fccc28becc778b26503bcb263479d128e0f66c8
owermaiden <ower.maiden@cybertekschool.com>
2021-11-03 01:11:58 +0300

    Spring Java Configuration

---
+++ Spring-08-JavaConfiguration/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,22 @@
+package com.cybertek;
+
+import com.cybertek.configs.CybertekAppConfig;
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+
+
+        Course course2 = container.getBean("java", Course.class);
+        course2.getTeachingHours();
+
+        System.out.println(course2.toString());
+    }
+
+
+}
---
+++ Spring-08-JavaConfiguration/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,37 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Scope("prototype")
+public class Java implements Course {
+
+    @Value("EU7")
+    private String batch;
+
+    @Value("${instructor}")
+    private String instructor;
+
+    @Value("${days}")
+    private String[] days;
+
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("this is java");
+    }
+}
