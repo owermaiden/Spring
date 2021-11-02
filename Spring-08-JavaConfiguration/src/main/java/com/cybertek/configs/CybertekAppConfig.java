b4c46130b9baf16c73e6b12b23ac2d369fdc3f51
owermaiden <ower.maiden@cybertekschool.com>
2021-11-03 01:40:23 +0300

    Spring Dependency Injection

--- Spring-08-JavaConfiguration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ Spring-08-JavaConfiguration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -1,18 +1,26 @@
 package com.cybertek.configs;
 
 import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
 import com.cybertek.services.Selenium;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.cybertek")
+@PropertySource("classpath:application.properties")
 public class CybertekAppConfig {
 
     // çok kullanılmıyor...third party class larda kullanılmış..Method level annotation
     @Bean
     public Selenium selenium(){
-        return new Selenium();
+        return new Selenium(officeHours());
+    }
+
+    @Bean
+    public OfficeHours officeHours(){
+        return new OfficeHours();
     }
 }
--- Spring-08-JavaConfiguration/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-08-JavaConfiguration/src/main/java/com/cybertek/services/Selenium.java
@@ -1,10 +1,23 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class Selenium implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Selenium(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    public Selenium() {
+
+    }
+
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly teaching hours : 40");
