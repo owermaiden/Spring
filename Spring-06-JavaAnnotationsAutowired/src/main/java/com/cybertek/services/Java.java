542896390674aa84e49b18b52761739ae76abc52
owermaiden <ower.maiden@cybertekschool.com>
2021-11-02 23:47:12 +0300

    Spring Dependency Injection

--- Spring-06-JavaAnnotationsAutowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaAnnotationsAutowired/src/main/java/com/cybertek/services/Java.java
@@ -1,22 +1,28 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
-    // we can also put autowired here as a field injection.............
-    //@Autowired
-    OfficeHours officeHours;
+    //@Autowired    // we can also put autowired here as a field injection.............
+    //OfficeHours officeHours;
+    // @Autowired
+    // @Qualifier("mockInterviewHours")  // dependent bean lerden hangisinin kullanılacağını seçiyoruz...
+    ExtraSessions extraSessions;
 
     // Constructor injection..There is no need if there is only one constructor...
+
     @Autowired
-    public Java(OfficeHours officeHours) {
-        this.officeHours = officeHours;
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
     }
 
+
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : " + officeHours.getHours());
+        System.out.println("Weekly teaching hours : " + extraSessions.getHours());
     }
 }
