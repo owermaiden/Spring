2790f55c6f36f4aa0ab8f8fc821405c373a2866d
owermaiden <ower.maiden@cybertekschool.com>
2021-11-02 09:51:34 +0300

    Spring Dependency Injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,9 +1,18 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
+
+    //OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching hours : 20");
+        System.out.println("Weekly Teaching hours : " + (20 + extraSessions.getHours()));
     }
 }
